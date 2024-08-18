47f1646897ae64702ffc364c470f8e5189f310d8
Rallenson <Rallenson90@gmail.com>
2024-08-18 17:40:26 -0300

    feat: Update @vercel/analytics dependency to version 1.3.1

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import Header from './Components/Layouts/Header/Header';
 import Footer from './Components/Layouts/Footer/Footer';
 import { ChakraProvider } from "@chakra-ui/react";
 import PortfolioTheme from './styles/thema';
+import { Analytics } from "@vercel/analytics/react"
 
 const rootElement = document.getElementById('root');
 
@@ -12,6 +13,7 @@ if(rootElement){
     ReactDOM.createRoot(rootElement).render(
         <React.StrictMode>
             <ChakraProvider theme={PortfolioTheme}>
+                <Analytics />
                 <Header />
                 <Footer />
             </ChakraProvider>
