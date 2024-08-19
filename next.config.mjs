6077cd2597581c460b6a1392f86ba100204f0251
Rallenson <Rallenson90@gmail.com>
2024-08-18 21:37:34 -0300

    chore: Update next.config.mjs to enable experimental appDir

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    srcDir: 'src',
+    experimental: {
+        appDir: true,
+    },
 };
 
 export default nextConfig;
