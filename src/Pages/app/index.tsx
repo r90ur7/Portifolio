e29f2043de08572267d9dab521860a369740a184
Rallenson <Rallenson90@gmail.com>
2024-08-18 23:37:18 -0300

    Refactorizar páginas de la aplicación

---
+++ src/Pages/app/index.tsx
@@ -0,0 +1,23 @@
+import { Box } from "@chakra-ui/react"
+import AboutMe from "../../Components/AboutMe"
+import Contact from "../../Components/Contact"
+import Experience from "../../Components/Experiencie"
+import Footer from "../../Components/Footer"
+import Hero from "../../Components/Hero"
+import Projects from "../../Components/Projects"
+import Skills from "../../Components/Skills"
+import React from "react"
+
+const IndexPage = () => (
+    <Box>
+        <Hero />
+        <AboutMe />
+        <Experience />
+        <Projects />
+        <Skills />
+        <Contact />
+        <Footer />
+    </Box>
+)
+
+export default IndexPage
