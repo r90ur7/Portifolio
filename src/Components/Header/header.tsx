531829c012a55dcbcae8f205597b20efb51c9cf4
Rallenson Silva <Rallenson900@gmail.com>
2025-02-05 15:29:54 -0300

    feat: Finalizando primeira versão do Header

--- src/Components/Header/header.tsx
+++ src/Components/Header/header.tsx
@@ -13,14 +13,6 @@ const Header = () => {
     const controls = useAnimation();
     const [prevScrollPos, setPrevScrollPos] = useState(0);
     const [visible, setVisible] = useState(true);
-    const { scrollY } = useScroll();
-
-
-    const pulse = keyframes`
-  0% { transform: scale(1); opacity: 0.6; }
-  50% { transform: scale(1.015); opacity: 0.8; }
-  100% { transform: scale(1); opacity: 0.6; }
-`;
 
     useEffect(() => {
         const handleScroll = () => {
@@ -28,7 +20,7 @@ const Header = () => {
             const isVisible = prevScrollPos > currentScrollPos;
 
             setVisible(
-                currentScrollPos < 50 ||
+                currentScrollPos < 100 ||
                 (isVisible && currentScrollPos > 100)
             );
             setPrevScrollPos(currentScrollPos);
@@ -48,7 +40,6 @@ const Header = () => {
 
     return (
         <MotionBox
-            p={8}
             maxWidth="1200px"
             mx="auto"
             position="sticky"
@@ -57,36 +48,46 @@ const Header = () => {
             animate={controls}
             initial={{ opacity: 0 }}
         >
-            <Flex flexWrap={"wrap"} justifyContent={"center"} alignContent={"space-around"} direction="row" gap={4}>
-                <VStack>
-                    {/* Nome com animação */}
+            <Flex
+                flexWrap="wrap"
+                justifyContent="center"
+                alignItems="center"
+                direction={{ base: "column", md: "row" }}
+                gap={{ base: 6, md: 12 }}
+            >
+                <VStack
+                    align={{ base: "center", md: "flex-start" }} // Alinhamento responsivo
+                    textAlign={{ base: "center", md: "left" }} // Texto centralizado em mobile
+                    spacing={4}
+                    maxW={{ base: "100%", md: "50%" }} // Largura máxima responsiva
+                >
                     <MotionHeading
                         as="h1"
-                        size="2xl"
+                        size={{ base: "xl", md: "2xl" }}
                         fontWeight="bold"
                         bgGradient="linear(to-t, #B650F2, #362558)"
-                        bgClip={"text"}
+                        bgClip="text"
                         filter="brightness(1.2)"
                         initial={{ opacity: 0, y: -20 }}
                         animate={{ opacity: 1, y: 0 }}
                         transition={{ duration: 0.8, delay: 0.2 }}
+                        textAlign={{ base: "center", md: "justify" }}
                     >
                         Rallenson Silva
                     </MotionHeading>
 
-                    {/* Cargo com animação */}
                     <MotionText
-                        fontSize="xl"
+                        fontSize={{ base: "lg", md: "xl" }}
                         color="gray.600"
                         fontWeight="medium"
                         initial={{ opacity: 0, x: -20 }}
                         animate={{ opacity: 1, x: 0 }}
                         transition={{ duration: 0.6, delay: 0.4 }}
+                        textAlign={"justify"}
                     >
                         Engenheiro de Software | Desenvolvedor Full Stack
                     </MotionText>
 
-                    {/* Descrição com animação */}
                     <MotionText
                         maxWidth="600px"
                         color="gray.500"
@@ -94,6 +95,8 @@ const Header = () => {
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         transition={{ duration: 0.8, delay: 0.6 }}
+                        textAlign={"justify"}
+
                     >
                         Desenvolvedor Full Stack especializado em React Next.js e .NET, com experiência em construção de sistemas educacionais escaláveis e interfaces intuitivas. Combino técnicas ágeis com arquitetura modular para entregas de alta performance.
                     </MotionText>
@@ -106,7 +109,7 @@ const Header = () => {
                         animate={{ scale: 1 }}
                         transition={{ type: "spring", stiffness: 260, damping: 20, delay: 0.8 }}
                     >
-                        <Flex gap={6}>
+                        <Flex flexWrap={"wrap"} gap={6} justifyContent={"center"}>
                             <Link href="#contact" _hover={{ textDecoration: 'none' }}>
                                 <IconButton
                                     as={motion.button}
@@ -159,10 +162,10 @@ const Header = () => {
                                 />
                             </Link>
                         </Flex>
-                        <Link href="https://bronze-stormy-55.tiiny.site" isExternal mt={4} width="100%">
+                        <Link display={"flex"} justifyContent={"center"} href="https://bronze-stormy-55.tiiny.site" isExternal mt={4} width="100%">
                             <IconButton
                                 as={motion.button}
-                                width="100%"
+                                width={["100px","100%", "50%", "100%" ]}
                                 aria-label="Portfólio"
                                 icon={< FaDownload color="#096666FF" />}
                                 variant="solid"
@@ -178,65 +181,68 @@ const Header = () => {
                         </Link>
                     </MotionFlex>
                 </VStack>
-                <VStack>
+                <VStack
+                    align="center"
+                    spacing={4}
+                    maxW={{ base: "100%", md: "40%" }}
+                >
                     <Flex>
                         <VStack>
-                            <Box position="relative" w="300px" h="300px">
+                            <Box
+                                position="relative"
+                                w={200}
+                                h={{ base: "100vw", md: "55vh" }}
+                                maxH={{ base: "6000px", md: "400px" }}
+                                borderRadius="md"
+                                overflow="hidden"
+                                boxShadow="2px 15px 50px 0.1px rgba(182, 80, 242, 0.25)"
+                                _before={{
+                                    content: '""',
+                                    position: "absolute",
+                                    top: "-2px",
+                                    left: "-2px",
+                                    right: "-2px",
+                                    bottom: "-2px",
+                                    borderRadius: "md",
+                                    background: "linear-gradient(to top, #B650F2 0%, #362558 100%)",
+                                    zIndex: 0,
+                                    filter: "blur(8px)",
+                                    opacity: 0.7,
+                                }}
+                                _after={{
+                                    content: '""',
+                                    position: "absolute",
+                                    top: 0,
+                                    left: 0,
+                                    right: 0,
+                                    bottom: 0,
+                                    borderRadius: "md",
+                                    border: "2px solid",
+                                    background: "linear-gradient(to top, #362558 0%, #B650F2 100%)",
+                                    borderColor: "rgba(182, 80, 242, 0.15)",
+                                    mixBlendMode: "soft-light",
+                                }}
+                            >
                                 <Box
-                                    position="relative"
-                                    w="350px"
-                                    h="400px"
-                                    borderRadius="md"
-                                    overflow="hidden"
-                                    boxShadow="2px 15px 50px 0.1px rgba(182, 80, 242, 0.25)"
-                                    _before={{
-                                        content: '""',
-                                        position: 'absolute',
-                                        top: '-2px',
-                                        left: '-2px',
-                                        right: '-2px',
-                                        bottom: '-2px',
-                                        borderRadius: 'md',
-                                        background: 'linear-gradient(to top, #B650F2 0%, #362558 100%)',
-                                        zIndex: 0,
-                                        filter: 'blur(8px)',
-                                        opacity: 0.7
+                                    as="img"
+                                    src="assets/perfil.png"
+                                    alt="Minha Foto"
+                                    position="absolute"
+                                    w="100%"
+                                    h="100%"
+                                    top="50%"
+                                    left="50%"
+                                    transform="translate(-50%, -50%)"
+                                    zIndex="1"
+                                    sx={{
+                                        "&": {
+                                            minWidth: "100%",
+                                            minHeight: "100%",
+                                            objectFit: "cover",
+                                            objectPosition: "center top",
+                                        },
                                     }}
-                                    _after={{
-                                        content: '""',
-                                        position: 'absolute',
-                                        top: 0,
-                                        left: 0,
-                                        right: 0,
-                                        bottom: 0,
-                                        borderRadius: 'md',
-                                        border: '2px solid',
-                                        background: 'linear-gradient(to top, ##362558 0%, #B650F2 100%)',
-                                        borderColor: 'rgba(182, 80, 242, 0.15)',
-                                        mixBlendMode: 'soft-light',
-                                    }}
-                                >
-                                    <Box
-                                        as="img"
-                                        src="assets/perfil.png"
-                                        alt="Minha Foto"
-                                        position="absolute"
-                                        w="auto"
-                                        h="180%"
-                                        top="60%"
-                                        left="50%"
-                                        transform="translate(-50%, -50%)"
-                                        zIndex="1"
-                                        sx={{
-                                            '&': {
-                                                minWidth: '100%',
-                                                minHeight: '100%',
-                                                objectFit: 'cover',
-                                                objectPosition: 'center top',
-                                            }
-                                        }}
-                                    />
-                                </Box>
+                                />
                             </Box>
                         </VStack>
                     </Flex>
